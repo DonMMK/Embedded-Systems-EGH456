07b0ba6c4bd16351852305adb2e7f75acd73e0d2
don <misurakalu@gmail.com>
2022-03-28 12:30:23 +1000

    Update timers.c

--- CodeComposerStudio/timers/timers.c
+++ CodeComposerStudio/timers/timers.c
@@ -92,6 +92,12 @@ uint32_t g_ui32SysClock;
 //*****************************************************************************
 uint32_t g_ui32Flags;
 
+
+volatile uint32_t Counter_Timer0 = 0;
+volatile uint32_t Counter_Timer1 = 0;
+char Store_Count0[10];
+char Store_Count1[10];
+
 //*****************************************************************************
 //
 // The error routine that is called if the driver library encounters an error.
@@ -113,7 +119,7 @@ void
 Timer0IntHandler(void)
 {
     char cOne, cTwo;
-
+    Counter_Timer0++;
     //
     // Clear the timer interrupt.
     //
@@ -148,7 +154,7 @@ void
 Timer1IntHandler(void)
 {
     char cOne, cTwo;
-
+    Counter_Timer1++;
     //
     // Clear the timer interrupt.
     //
@@ -261,10 +267,7 @@ int
 main(void)
 {
     // Initialize a counter for timers 0 and 1
-    volatile uint32_t Counter_Timer0 = 0;
-    volatile uint32_t Counter_Timer1 = 0;
-    char Store_Count0[10];
-    char Store_Count1[10];
+
     uint32_t counter = 0;
 
     //
